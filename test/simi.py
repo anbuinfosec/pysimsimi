c2da6219f93434a6c377bf6df523fc5fdb5b4033
Mohammad Alamin <anbuinfosec@gmail.com>
2025-05-15 01:55:08 +0600

    first commit

---
+++ test/simi.py
@@ -0,0 +1,5 @@
+from pysimsimi import talk, teach
+response = talk("Hi", "en")
+print("Simi says:", response)
+teach_response = teach("Hi", "Hello there!", "en")
+print("Teaching result:", teach_response)
